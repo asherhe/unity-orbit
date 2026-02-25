d7ee343a1ea595bed5ef2692a3f73984b67b7c5f
asherhe <github@asherhe.com>
2026-02-24 17:19:22 -0800

    establish proper draw order of map labels

--- Assets/Scripts/UI/MapView/MapLabelManager.cs
+++ Assets/Scripts/UI/MapView/MapLabelManager.cs
@@ -22,11 +22,33 @@ namespace UI
         [SerializeField]
         private GameObject _spacecraftLabelPrefab;
 
+        // gameobjects that each hold a label type under one object, used to establish proper draw order
+        private Transform _apsisContainer, _SOITransitionContainer, _encounterContainer, _celestialBodyContainer, _spacecraftContainer;
+
         private readonly HashSet<MapLabel> _labels = new();
 
         protected override void Awake()
         {
             base.Awake();
+
+            // spacecraft > celestial body > SOI transition > apses > encounters
+            // instantiate in reverse order because draw order shows newest first
+
+            _encounterContainer = new GameObject("Encounter Label Group Container").transform;
+            _encounterContainer.SetParent(transform, worldPositionStays: false);
+
+            _apsisContainer = new GameObject("Apsis Label Container").transform;
+            _apsisContainer.SetParent(transform, worldPositionStays: false);
+
+            _SOITransitionContainer = new GameObject("SOI Transition Label Container").transform;
+            _SOITransitionContainer.SetParent(transform, worldPositionStays: false);
+
+            _celestialBodyContainer = new GameObject("Celestial Body Label Container").transform;
+            _celestialBodyContainer.SetParent(transform, worldPositionStays: false);
+
+            _spacecraftContainer = new GameObject("Spacecraft Label Container").transform;
+            _spacecraftContainer.SetParent(transform, worldPositionStays: false);
+
 
             MapViewManager.WhenInstantiated(() =>
             {
@@ -46,7 +68,7 @@ namespace UI
 
         public ApsisLabel AddApsis(OrbitState orbit, ApsisLabel.DisplayMode mode)
         {
-            var labelObject = Instantiate(_apsisLabelPrefab, transform);
+            var labelObject = Instantiate(_apsisLabelPrefab, _apsisContainer);
             var apsisLabel = labelObject.GetComponent<ApsisLabel>();
             apsisLabel.Orbit = orbit;
             apsisLabel.Mode = mode;
@@ -54,27 +76,27 @@ namespace UI
         }
         public SOITransitionLabel AddSOITransition(Patch patch)
         {
-            var labelObject = Instantiate(_SOITransitionLabelPrefab, transform);
+            var labelObject = Instantiate(_SOITransitionLabelPrefab, _SOITransitionContainer);
             var soiLabel = labelObject.GetComponent<SOITransitionLabel>();
             soiLabel.Patch = patch;
             return soiLabel;
         }
         public EncounterLabelGroup AddEncounterLabelGroup()
         {
-            var labelObject = Instantiate(_encounterLabelGroupPrefab, transform);
+            var labelObject = Instantiate(_encounterLabelGroupPrefab, _encounterContainer);
             return labelObject.GetComponent<EncounterLabelGroup>();
         }
 
         public CelestialBodyLabel AddCelestialBody(CelestialBody body)
         {
-            var labelObject = Instantiate(_celestialBodyLabelPrefab, transform);
+            var labelObject = Instantiate(_celestialBodyLabelPrefab, _celestialBodyContainer);
             labelObject.name = $"{body.bodyName} Label";
             return (CelestialBodyLabel)SetupObjectLabel(labelObject, body);
         }
 
         public SpacecraftLabel AddSpacecraft(Spacecraft craft)
         {
-            var labelObject = Instantiate(_spacecraftLabelPrefab, transform);
+            var labelObject = Instantiate(_spacecraftLabelPrefab, _spacecraftContainer);
             labelObject.name = $"{craft.craftName} Label";
             return (SpacecraftLabel)SetupObjectLabel(labelObject, craft);
         }
