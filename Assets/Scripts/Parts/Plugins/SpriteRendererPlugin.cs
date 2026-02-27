e80555991e754654af22b1d01c09a521ab393522
asherhe <github@asherhe.com>
2026-02-26 22:56:35 -0800

    turn off the sun when it's night time

--- Assets/Scripts/Parts/Plugins/SpriteRendererPlugin.cs
+++ Assets/Scripts/Parts/Plugins/SpriteRendererPlugin.cs
@@ -60,12 +60,27 @@ namespace Parts
             base.OnUpdate();
 
             // assume position within craft has negligible effect
-            var heliocentric = craft.orbit.body.GetHeliocentricPosition() + craft.Position;
+            var heliocentric = craft.body.GetHeliocentricPosition() + craft.Position;
             var sunIntensity = (float)(1.7e22 / heliocentric.Magnitude2); // slightly over 2 at earth orbit
-            Vector4 sunDirection = -heliocentric.Normalized;
-            sunDirection.z = 0.3f; sunDirection.w = 0.0f;
+            var sunDirection = -heliocentric.Normalized;
+            // set z to 0.3 so that parts are better illuminated
+            Vector4 sunDir = new((float)sunDirection.x, (float)sunDirection.y, 0.3f, 0.0f);
             _spriteRenderer.material.SetFloat("_SunIntensity", sunIntensity);
-            _spriteRenderer.material.SetVector("_SunDir", sunDirection);
+            _spriteRenderer.material.SetVector("_SunDir", sunDir);
+
+            // find closest point along sun ray to parent body
+            var t = Math.Max(-Vector2d.Dot(sunDirection, craft.Position), 0.0);
+            var closest = craft.Position + t * sunDirection;
+            // altitude of closest point
+            var alt = closest.Magnitude - craft.body.radius;
+            // apparent radius of the sun, if we projected it to the closest point
+            var apparentRad = CelestialBodyManager.Instance.celestialBodies["Sun"].radius / heliocentric.Magnitude * t;
+            // how much of the sun is occluded, used for soft shadows
+            // not completely physically accurate but good enough to give convincing results
+            var occlusion = 0.5 * (1.0 + alt / apparentRad);
+            var shade = Mathf.Clamp01((float)occlusion);
+            // deal with atmospheric scattering at a later date
+            _spriteRenderer.material.SetColor("_SunColor", new Color(shade, shade, shade, 1f));
         }
     }
 }
