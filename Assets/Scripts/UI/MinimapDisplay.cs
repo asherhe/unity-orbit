6fd11bbc9bc0d709470c5a2d9a2207babfef38ec
asherhe <github@asherhe.com>
2026-03-01 14:34:25 -0800

    automatically infer MinimapDisplay inactive location

--- Assets/Scripts/UI/MinimapDisplay.cs
+++ Assets/Scripts/UI/MinimapDisplay.cs
@@ -7,24 +7,29 @@ namespace UI
 {
     public class MinimapDisplay : MonoBehaviour
     {
+        private RectTransform rectTransform;
         public bool IsMinimapActive { get => MapViewManager.Instance.activeView == CameraView.FlightView; }
 
-        private RectTransform _rectTransform;
+        private Vector2 activePos, inactivePos;
+        private Vector2 TargetPos => IsMinimapActive ? activePos : inactivePos;
 
         private void Awake()
         {
-            _rectTransform = GetComponent<RectTransform>();
-            MapViewManager.WhenInstantiated(() => {
+            rectTransform = transform as RectTransform;
+            activePos = rectTransform.anchoredPosition;
+            inactivePos = rectTransform.anchoredPosition + Vector2.right * (rectTransform.rect.width + 40f);
+            rectTransform.anchoredPosition = TargetPos;
+
+            MapViewManager.WhenInstantiated(() =>
+            {
                 MapViewManager.Instance.OnMapToggled += ToggleMinimap;
             });
         }
 
         private void ToggleMinimap()
         {
-            _rectTransform.DOAnchorPosX(
-               IsMinimapActive ? -10.0f : 200.0f,
-                0.25f
-            ).SetEase(IsMinimapActive ? Ease.OutCubic : Ease.InCubic);
+            rectTransform.DOAnchorPos(TargetPos, 0.25f)
+                .SetEase(IsMinimapActive ? Ease.OutCubic : Ease.InCubic);
         }
     }
 }
